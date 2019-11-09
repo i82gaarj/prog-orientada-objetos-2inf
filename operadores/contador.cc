0bdc2b17797e34cf3e67533d18877fb5dbdcb400
Jaime García <i82gaarj@uco.es>
2019-11-09 14:32:16 +0100

    Finalizada P4 operadores

--- operadores/contador.cc
+++ operadores/contador.cc
@@ -1,11 +1,196 @@
 #include "contador.h"
+#include <list>
 
-Contador :: Contador operator = (const Contador &c){
-	Contador aux;
-	aux.valor_ = c.valor_;
+Contador::Contador(int valor, int min, int max):max_(max), min_(min){
+	ncambios_ = 0;
+	if (valor <= max && valor >= min){
+		valor_ = valor;
+	}
+	else if (max <= min){
+		valor_ = 0;
+		min_ = 0;
+		max_ = 1000;
+	}
+	else {
+		valor_ = 0;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+}
+
+Contador Contador::operator = (const Contador &c){
+	setValor(c.get());
+	setMin(c.getMin());
+	setMax(c.getMax());
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+
+	return *this;
+}
+
+Contador Contador::operator = (const int &i){
+	if (i >= max_){
+		valor_ = max_;
+	}
+	else if (i <= min_){
+		valor_ = min_;
+	}
+	else{
+		valor_ = i;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+
+	return *this;
+}
+
+Contador Contador::operator ++ (int){
+	Contador aux = *this;
+
+	if (valor_ >= max_){
+		valor_ = max_;
+	}
+	else{
+		valor_++;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+
 	return aux;
 }
 
-Contador :: Contador operator = (){
+Contador Contador::operator -- (void){
+	if (valor_ <= min_){
+		valor_ = min_;
+	}
+	else{
+		--valor_;
+	}
 
+	ncambios_++;
+	cambios_.push_back(valor_);
+
+	return *this;
+}
+
+Contador Contador::operator ++ (void){
+	if (valor_ >= max_){
+		valor_ = max_;
+	}
+	else{
+		++valor_;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+
+	return *this;
+}
+
+Contador Contador :: operator -- (int){ // postfijo
+	Contador aux = *this;
+	if (valor_ <= min_){
+		valor_ = min_;
+	}
+	else{
+		valor_--;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+
+	return aux;
+}
+
+Contador operator + (int x, Contador c){
+	Contador aux(c);
+	int nuevo_valor = x + aux.get();
+
+	if (nuevo_valor > aux.getMax()){
+		aux.setValor(aux.getMax());
+	}
+	else if (nuevo_valor < aux.getMin()){
+		aux.setValor(aux.getMin());
+	}
+	else{
+		aux.setValor(nuevo_valor);
+	}
+
+	aux.ncambios_++;
+	aux.cambios_.push_back(aux.valor_);
+
+	return aux;
+}
+
+Contador Contador :: operator + (int x){ // c + x
+	int nuevo_valor = valor_ + x;
+	if ((nuevo_valor) < min_){
+		valor_ = min_;
+	}
+	else if ((nuevo_valor) > max_){
+		valor_ = max_;
+	}
+	else{
+		valor_ = nuevo_valor;
+	}
+	ncambios_++;
+	cambios_.push_back(valor_);
+	return *this;
+}
+
+Contador Contador :: operator - (int x){ // c - x
+	int nuevo_valor = valor_ - x;
+
+	if ((nuevo_valor) < min_){
+		valor_ = min_;
+	}
+	else if ((nuevo_valor) > max_){
+		valor_ = max_;
+	}
+	else{
+		valor_ = nuevo_valor;
+	}
+
+	ncambios_++;
+	cambios_.push_back(valor_);
+	
+	return *this;
+}
+
+Contador operator - (int x, Contador c){
+	Contador aux(c);
+	int nuevo_valor = x - aux.get();
+
+	if (nuevo_valor > aux.getMax()){
+		aux.setValor(aux.getMax());
+	}
+	else if (nuevo_valor < aux.getMin()){
+		aux.setValor(aux.getMin());
+	}
+	else{
+		aux.setValor(x);
+	}
+
+	aux.ncambios_++;
+	aux.cambios_.push_back(aux.valor_);
+
+	return aux;
+}
+
+bool Contador :: undo(int x){
+	if (x > (ncambios_ - 1) || x < 1){
+		return false;
+	}
+	else{
+		for (int i = 0; i < x; i++){
+			cambios_.pop_back();
+			valor_ = cambios_.back();
+			ncambios_--;
+		}
+		return true;
+	}
 }
--- operadores/contador.h
+++ operadores/contador.h
@@ -1,3 +1,4 @@
+#include <list>
 
 using namespace std;
 
@@ -7,28 +8,53 @@ private:
 	int max_;
 	int min_;
 
-public:
-	Contador(int valor = 0, int min = 0, int max = 1000):max_(max), min_(min){
-		if (valor <= max && valor >= min){
-			valor_ = valor;
-		}
-		else {
-			valor_ = 0;
-		}
-	}
+	int ncambios_;
+	list <int> cambios_;
 
-	Contador operator = (int valor);
+public:
+	Contador(int valor = 0, int min = 0, int max = 1000);
+
+	Contador operator = (const int &i);
+
 	Contador operator = (const Contador &c);
-	Contador operator ++ (const Contador &c);
-	Contador operator -- (const Contador &c);
-	friend Contador operator + (const Contador &c, int x);
-	friend Contador operator + (int x, const Contador &c);
-	Contador operator - ();
-	Contador operator - ();
+
+	Contador operator ++ (void);
+
+	Contador operator -- (void);
+
+	Contador operator ++ (int);
+
+	Contador operator -- (int);
+
+	Contador operator + (int x);
+	friend Contador operator + (int x, Contador c);
+
+	Contador operator - (int x);
+	friend Contador operator - (int x, Contador c);
 
 	bool undo(int n = 1);
 
-	inline int get(){
+	inline int get() const{
 		return valor_;
+	}
+
+	inline void setValor(int valor){
+		valor_ = valor;
+	}
+
+	inline int getMin() const{
+		return min_;
+	}
+
+	inline int getMax() const{
+		return max_;
+	}
+
+	inline void setMin(int min){
+		min_ = min;
+	}
+
+	inline void setMax(int max){
+		max_ = max;
 	}
 };
