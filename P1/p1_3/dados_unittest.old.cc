b9c67c2c23c99aacecacc53d0552384c414d8a6f
Jaime García <i82gaarj@uco.es>
2019-11-09 15:06:11 +0100

    Añadidas P1, P2 y P3

---
+++ P1/p1_3/dados_unittest.old.cc
@@ -0,0 +1,93 @@
+// dados_unittest.cc: Juan A. Romero
+// A sample program demonstrating using Google C++ testing framework.
+//
+
+
+// This sample shows how to write a more complex unit test for a class
+// that has multiple member functions.
+//
+// Usually, it's a good idea to have one test for each method in your
+// class.  You don't have to do that exactly, but it helps to keep
+// your tests organized.  You may also throw in additional tests as
+// needed.
+
+#include "dados.h"
+#include "gtest/gtest.h"
+
+// Tests the c'tor.
+TEST(Dados, Constructor) {
+  Dados d;
+  
+  EXPECT_EQ(1, d.getDado1());
+  EXPECT_EQ(1, d.getDado2());
+  EXPECT_EQ(2, d.getSuma());
+  
+}
+
+// Tests operación lanzamiento
+TEST(Dados, Lanzamiento) {
+  Dados d;
+  for (int i=0; i<100; i++){
+  d.lanzamiento();
+  EXPECT_GT(d.getDado1(), 0);
+  EXPECT_LT(d.getDado1(), 7);
+  EXPECT_GT(d.getDado2(), 0);
+  EXPECT_LT(d.getDado2(), 7);
+}
+
+}
+
+// Tests operación suma
+TEST(Dados, Suma) {
+  Dados d;
+  
+  EXPECT_EQ(d.getDado1()+d.getDado2(), d.getSuma());
+}
+
+// Tests modificadores
+TEST(Dados, Modificadores) {
+  Dados d;
+  
+  EXPECT_FALSE(d.setDado1(9));
+  EXPECT_FALSE(d.setDado1(-9));
+  EXPECT_FALSE(d.setDado2(9));
+  EXPECT_FALSE(d.setDado2(-9));
+  d.setDado1(3);
+  EXPECT_EQ(3, d.getDado1());
+  d.setDado2(2);
+  EXPECT_EQ(2, d.getDado2());
+  EXPECT_EQ(5, d.getSuma());
+}
+
+TEST(Dados, Diferencia){
+	Dados d;
+
+	for (int i = 0; i < 1000; i++){
+
+		d.lanzamiento();
+
+		int valor_d1 = d.getDado1();
+		int valor_d2 = d.getDado2();
+
+		EXPECT_EQ(d.getDiferencia(), abs(valor_d2 - valor_d1));
+
+	}
+}
+
+TEST(Dados, getLanzamientos){
+	Dados d;
+
+	EXPECT_EQ(0, d.getLanzamientos1());
+	EXPECT_EQ(0, d.getLanzamientos2());
+
+
+	int n = 1000;
+
+	for (int i = 0; i < n; i++){
+		d.lanzamiento();
+	}
+
+	EXPECT_EQ(n, d.getLanzamientos1());
+	EXPECT_EQ(n, d.getLanzamientos2());
+
+}
---
+++ P3/ruleta.h
@@ -0,0 +1,83 @@
+#ifndef RULETA_H
+#define RULETA_H
+
+#include <list>
+#include "jugador.h"
+#include "crupier.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class Ruleta{
+private:
+	int banca_;
+	int bola_;
+	list <Jugador> jugadores_;
+	Crupier crupier_;
+
+public:
+	Ruleta(Crupier c):crupier_(c){
+		banca_ = 1000000;
+		bola_ = -1;
+		srand(time(NULL));
+	}
+
+	inline int getBanca() const{
+		return banca_;
+	}
+
+	inline bool setBanca(int banca){
+		if (banca > 0){
+			banca_ = banca;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	inline int getBola() const{
+		return bola_;
+	}
+
+	bool setBola(int bola){
+		if (bola >= 0 && bola <= 36){
+			bola_ = bola;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	inline const Crupier getCrupier(){
+		return crupier_;
+	}
+
+	inline void setCrupier(Crupier crupier){
+		crupier_ = crupier;
+	}
+
+	inline const list <Jugador> getJugadores(){
+		return jugadores_;
+	}
+
+	bool addJugador(Jugador jugador);
+
+	int deleteJugador(string dni);
+	int deleteJugador(Jugador jugador);
+
+	void escribeJugadores();
+	void leeJugadores();
+
+	inline void giraRuleta(){
+		setBola(rand() % 37);
+	}
+	
+	void getPremios();
+
+	void getPremiosJugador(list <Jugador> :: iterator player, int &gananciasBanca);
+
+};
+
+#endif
